6bc01b1b62651a0a00c703365aa00c92d09ddbf5
sunilwane <sunilwane211@gmail.com>
2024-07-22 11:25:53 -0700

    backend_folder
    Signed-off-by: sunilwane <sunilwane211@gmail.com>

---
+++ backend/data.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+const URL = process.env.MONGO_URL;
+const connection = mongoose.connect(URL);
+
+module.exports = {
+  connection,
+};
