622bc10efc252a639325f3bf43d22bf85b065a5a
Mahesh Patil <nitro56565@gmail.com>
2024-07-23 00:38:31 +0530

    database connectivity setup
    Signed-off-by: Mahesh Patil <nitro56565@gmail.com>

--- backend/data.js
+++ backend/data.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
+
 const URL = process.env.MONGO_URL;
 const connection = mongoose.connect(URL);
 
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,26 @@
 const express = require("express");
-const connection = require("./data");
+const { connection } = require("./data");
 const app = express();
+
 app.use(express.json());
+
 app.get("/user", (req, res) => {
   res.send("life matters");
 });
-app.listen(8083, async (req, res) => {
-  await connection;
-  console.log("db is conncected....");
-  console.log("server is listeining....");
+
+app.listen(8083, () => {
+  connection
+    .then(() => {
+      console.log("db is connected....");
+      console.log("server is listening on port 8083....");
+    })
+    .catch((error) => {
+      console.error("Failed to connect to the database:", error);
+      process.exit(1);
+    });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
 });
