13ed77445b0b5fdbc37f5d1879a51594db6ade3d
Mahesh Patil <nitro56565@gmail.com>
2024-07-23 21:58:09 +0530

    cors added vercel.json updated
    Signed-off-by: Mahesh Patil <nitro56565@gmail.com>

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const { connection } = require("./data");
 const app = express();
+const cors = require("cors");
 
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
-
+app.get("life-matters.vercel.app/", (req, res) => res.send("Express on Vercel"));
+app.use(cors(
+  {
+    origin: ["https://life-matters.vercel.app"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  }
+));
 
 app.get("/user", (req, res) => {
   res.send("life matters");
