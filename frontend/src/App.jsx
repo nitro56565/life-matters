7345bf8cae0bb8735e3446b0509b5fc190115324
Mahesh Patil <nitro56565@gmail.com>
2024-09-06 15:28:01 +0530

    vercel speedInsights implimentated
    Signed-off-by: Mahesh Patil <nitro56565@gmail.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import Routing from './Routing';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function App() {
   return (
       <>
         <Routing />
         <Analytics />
+        <SpeedInsights />
       </>
   )
 }
