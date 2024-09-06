70e11c76fb9e1b9c1c779bb872be7780da2ea238
Mahesh Patil <nitro56565@gmail.com>
2024-09-06 13:25:00 +0530

    vercel analytics added
    Signed-off-by: Mahesh Patil <nitro56565@gmail.com>

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Analytics } from "@vercel/analytics/react"
 import App from './App.jsx'
 import './index.css'
 
