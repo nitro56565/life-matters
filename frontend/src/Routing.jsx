f82f74e16584c0875a21ed2943497ef02f446529
nitro56565 <nitro56565@gmail.com>
2024-10-20 19:22:20 +0530

    refresh code
    Signed-off-by: nitro56565 <nitro56565@gmail.com>

--- frontend/src/Routing.jsx
+++ frontend/src/Routing.jsx
@@ -2,12 +2,10 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import HomePage from "./pages/HomePage";
-import Ambulance from "./pages/Ambulance";
 import TrafficPoliceSignUp from "./pages/signUp/TrafficPoliceSignUp";
 import AmbulanceSignIn from "./pages/siginIn/AmbulanceSignIn";
 import TrafficPoliceSignIn from "./pages/siginIn/TrafficPoliceSignIn";
 import AmbulanceSignUp from "./pages/signUp/AmbulanceSignUp";
-import TrafficPolice from "./pages/TrafficPolice";
 import AmbulanceMainPage from "./pages/ambulance-main-page/AmbulanceMainPage";
 import TrafficPoliceMainPage from "./pages/trafficPolice-main-page/TrafficPoliceMainPage";
 import ProtectedRoute from "./components/ProtectedRoute";
@@ -22,8 +20,6 @@ function Routing() {
       <Route path="/ambulancesignup" element={<AmbulanceSignUp />} />
       <Route path="/trafficpolicesignin" element={<TrafficPoliceSignIn />} />
       <Route path="/trafficpolicesignup" element={<TrafficPoliceSignUp />} />
-      <Route path="/ambulance" element={<Ambulance />} />
-      <Route path="/traffic-police" element={<TrafficPolice />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
 
       {/* Protected Routes */}
